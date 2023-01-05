3f390f7f99d42e53a3494ec66f1276d53bd390d4
Manvi Gupta <manvigupta226@gmail.com>
2023-01-05 11:08:15 +0530

    connection check

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ app.use(express.json());
 app.get("/", (req, res) => res.json({ message: "Hello World" }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(
+  "postgresql://postgres:F9CXfrtNCJWuGjAeRbaL@containers-us-west-16.railway.app:7851/railway"
+);
+
+// TO TEST THE CONNECTION
+
+sequelize
+
+  .authenticate()
+
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
